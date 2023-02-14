263ba537d595cd86ba3e83b0432ebd90d2c568ba
Gustavo Medeiros <gugamedeiros517@gmail.com>
2023-02-14 18:21:07 -0300

    docs: adds jsdoc to User class.

--- User.js
+++ User.js
@@ -1,6 +1,17 @@
+/**
+ * @author: Gustavo Medeiros
+ * This is an example of class which simulates a User.
+ */
+
 import generateHash from "./generateHash.js";
 
 class User {
+  /**
+   * Class constructor.
+   * Password is stored as a hash.
+   * @param username
+   * @param password
+   */
   constructor(username, password) {
     this.username = username;
     this.password = generateHash(password);
